c0116c67321a27ecd963e571330dee08c2d9556b
if1live <libsora25@gmail.com>
2019-07-29 16:55:34 +0900

    fix: external from package.json

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,38 +3,58 @@ import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
+import nodeBuiltins from 'rollup-plugin-node-builtins';
+import nodeGlobals from 'rollup-plugin-node-globals';
 import json from 'rollup-plugin-json'
-import builtins from 'builtin-modules'
 import copy from 'rollup-plugin-copy'
+import builtins from 'builtin-modules'
+
 
 const pkg = require('./package.json')
+const external = Object.keys(pkg.dependencies);
 
 const libraryName = 'nadeshiko'
+
+const globals = {
+	'express': 'express',
+	'dayjs': 'dayjs',
+	'yup': 'yup',
+	'http-errors': 'createError',
+	'js-yaml': 'yaml',
+	'lodash': '_',
+	'googleapis': 'googleapis',
+	'path': 'path',
+	'assert': 'assert',
+}
 
 export default {
 	input: `src/${libraryName}.ts`,
 	output: [
-		{ file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-		{ file: pkg.module, format: 'es', sourcemap: true },
+		{
+			file: pkg.main,
+			name: camelCase(libraryName),
+			format: 'umd',
+			sourcemap: true,
+			globals
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			sourcemap: true,
+			globals,
+		},
 	],
 	// Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
 	external: [
 		...builtins,
-		'dayjs',
-		'express',
-		'expressmy-error-handler',
-		'googleapis',
-		'httperrors',
-		'ioredis',
-		'jsyaml',
-		'lodash',
-		'mz',
-		'yup',
+		...external,
 	],
 	watch: {
 		include: 'src/**',
 	},
 	plugins: [
+		nodeBuiltins(),
+		nodeGlobals(),
 		// Allow json resolution
 		json(),
 		// Compile TypeScript files
@@ -59,6 +79,6 @@ export default {
 			targets: [
 				{ src: ['public/index.html', 'public/main.js'], dest: 'dist/public' },
 			]
-		})
+		}),
 	],
 }
